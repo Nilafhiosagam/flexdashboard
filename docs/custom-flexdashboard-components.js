7d889bca367d4c48d6345e8dc5a5f5f634e2ec40
Nilafhiosagam <vinniegaul@gmail.com>
2020-10-06 15:33:50 +0100

    daily update

---
+++ docs/custom-flexdashboard-components.js
@@ -0,0 +1,10 @@
+// gt tables
+window.FlexDashboardComponents.push({
+  find: function(container) {
+    return container.find('.gt-no-scroll');
+  },
+  flex: function(fillPage) {
+    return fillPage;
+  }
+});
+
